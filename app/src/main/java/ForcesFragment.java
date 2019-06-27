60d9c6186034eedc6919f564c6a8c41a9629eda4
Shyam <m.shyam.tnj@gmail.com>
2019-06-27 21:23:17 +0530

    Initialize project

---
+++ app/src/main/java/ForcesFragment.java
@@ -0,0 +1,22 @@
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.shyamking.deathapp.R;
+
+public class ForcesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_forces, container, false);
+        TextView t = v.findViewById(R.id.textView);
+        t.setText("Fragment 1");
+
+        return v;
+    }
+}
